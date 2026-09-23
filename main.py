596f685622985fe107a7055e3e4ec8ccf6e36615
Tejal Goyal <f20251726@pilani.bits-pilani.ac.in>
2026-09-23 21:38:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ import numpy as np
 from gesture_recog import fingers_up, classify_gesture, detect_swipe
 
 # Initial Setup
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0) #continuously provides video frames of default cam.
 
 # Screen size
 screen_width, screen_height = pyautogui.size()
 
 # Safety settings
-pyautogui.FAILSAFE = True
-pyautogui.PAUSE = 0
+pyautogui.FAILSAFE = True #stopd when the cursor reaches the top-left corner.
+pyautogui.PAUSE = 0 # gives a pause between two pyautogui commands.
 
 # MediaPipe setup
-mp_hands = mp.solutions.hands
-mp_draw = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands 
+mp_draw = mp.solutions.drawing_utils 
 
 hands = mp_hands.Hands(
-    max_num_hands=1,
-    min_detection_confidence=0.7,
-    min_tracking_confidence=0.7
+    max_num_hands=1, 
+    min_detection_confidence=0.7, #confidence required for a new hand detection
+    min_tracking_confidence=0.7 #landmark tracking model to follow the hand across consecutive frames
 )
 
 # Cursor settings
@@ -42,20 +42,20 @@ while True:
         break
 
     # Mirror webcam
-    frame = cv2.flip(frame, 1)
+    frame = cv2.flip(frame, 1) #horizontally flips the video frame
 
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #converts BGR to RGB
 
-    results = hands.process(rgb)
+    results = hands.process(rgb)# mediapipe analyzes the frame
 
     if results.multi_hand_landmarks:
 
         for hand_landmarks, handedness in zip(
-            results.multi_hand_landmarks,
-            results.multi_handedness
+            results.multi_hand_landmarks, #landmarks
+            results.multi_handedness #handlabels
         ):
 
-            hand_label = handedness.classification[0].label
+            hand_label = handedness.classification[0].label #returns right or left
 
             # Landmarks
             mp_draw.draw_landmarks(
@@ -69,14 +69,14 @@ while True:
             # Index fingertip
             index_tip = hand_landmarks.landmark[8]
 
-            x = int(index_tip.x * w)
+            x = int(index_tip.x * w) #convert coordinates in pixels
             y = int(index_tip.y * h)
 
             # Tracking history for swipe
             history.append((x, y))
 
             if len(history) > 10:
-                history.pop(0)
+                history.pop(0) #creates a sliding window
 
             cv2.circle(frame, (x, y), 10, (0, 255, 0), -1)
 
@@ -107,7 +107,7 @@ while True:
             )
 
             # Smooth movement
-            smooth_x = prev_x + (mouse_x - prev_x) * SMOOTHING
+            smooth_x = prev_x + (mouse_x - prev_x) * SMOOTHING #The cursor gradually moves instead of jumping
             smooth_y = prev_y + (mouse_y - prev_y) * SMOOTHING
 
             prev_x = smooth_x
@@ -115,11 +115,11 @@ while True:
 
             # Moving cursor with open palm only
             if gesture == "Open Palm":
-                pyautogui.moveTo(smooth_x, smooth_y)
+                pyautogui.moveTo(smooth_x, smooth_y) #update the mouse pointer's position on screen in real time."
 
             # Displaying Text
 
-            cv2.putText(
+            cv2.putText( #text onto an image. image, text, position, font, scale, color, thickness
                 frame,
                 f"Gesture: {gesture} ({confidence}%)",
                 (20, 80),
@@ -141,9 +141,9 @@ while True:
 
     else:
 
-        history.clear()
+        history.clear() #prevents false positives
 
-        cv2.putText(
+        cv2.putText(  
             frame,
             "No Hand Detected",
             (20, 40),
@@ -155,7 +155,7 @@ while True:
 
     h, w = frame.shape[:2]
 
-    cv2.rectangle(
+    cv2.rectangle( #tracking box
         frame,
         (FRAME_MARGIN, FRAME_MARGIN),
         (w - FRAME_MARGIN, h - FRAME_MARGIN),
@@ -163,11 +163,11 @@ while True:
         2
     )
 
-    cv2.imshow("Stark's Workstation", frame)
+    cv2.imshow("Stark's Workstation", frame) #displays the processed frame
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 # Cleanup and closing 
-camera.release()
-cv2.destroyAllWindows()
+camera.release() #Releases the webcam
+cv2.destroyAllWindows() #Closes every OpenCV window
